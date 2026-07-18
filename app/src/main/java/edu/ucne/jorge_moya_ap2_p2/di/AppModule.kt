99eeba5c2d3fe5e5031172be7e7a42c55556f2db
jorge <jorge@LAPTOP-D8GDLQRL>
2026-07-18 17:48:15 -0400

    Agrega AppModule con los dos primeros metodos

---
+++ app/src/main/java/edu/ucne/jorge_moya_ap2_p2/di/AppModule.kt
@@ -0,0 +1,35 @@
+package edu.ucne.jorge_moya_ap2_p2.di
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import edu.ucne.jorge_moya_ap2_p2.data.remote.Api
+import jakarta.inject.Singleton
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideMoshi(): Moshi {
+        return Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideApi(moshi: Moshi): Api {
+        return Retrofit.Builder()
+            .baseUrl("")
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+            .create(Api::class.java)
+    }
+}
