e52b78a1d0b4f6ac87ad13f12817fdd0639ff6bf
jorge <jorge@LAPTOP-D8GDLQRL>
2026-07-22 19:37:44 -0400

    Trabaja el view model del listado

---
+++ app/src/main/java/edu/ucne/jorge_moya_ap2_p2/presentation/model/list/GastoListViewModel.kt
@@ -0,0 +1,45 @@
+package edu.ucne.jorge_moya_ap2_p2.presentation.model.list
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import edu.ucne.jorge_moya_ap2_p2.data.remote.Resource
+import edu.ucne.jorge_moya_ap2_p2.domain.usecase.GetGastosUseCase
+import jakarta.inject.Inject
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+
+@HiltViewModel
+class GastoListViewModel @Inject constructor(
+    private val getGastosUseCase: GetGastosUseCase
+) : ViewModel() {
+
+    private val _state = MutableStateFlow(GastoListUiState())
+    val state = _state.asStateFlow()
+
+    init { loadGastos() }
+
+    fun onEvent(event: GastoListUiEvent) {
+        when (event) {
+            GastoListUiEvent.LoadGastos -> loadGastos()
+        }
+    }
+
+    private fun loadGastos() {
+        viewModelScope.launch {
+            getGastosUseCase().collect { result ->
+                when (result) {
+                    is Resource.Loading -> _state.update { it.copy(isLoading = true) }
+                    is Resource.Success -> _state.update {
+                        it.copy(isLoading = false, gastos = result.data ?: emptyList())
+                    }
+                    is Resource.Error -> _state.update {
+                        it.copy(isLoading = false, error = result.message)
+                    }
+                }
+            }
+        }
+    }
+}
