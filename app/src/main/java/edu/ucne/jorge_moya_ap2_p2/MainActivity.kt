27569de9cffb9bb70a3bbf3faf8471076ce6813e
jorge <jorge@LAPTOP-D8GDLQRL>
2026-07-22 19:40:16 -0400

    Llama al navigation dentro del main activity para que ejecute correctamente al app

--- app/src/main/java/edu/ucne/jorge_moya_ap2_p2/MainActivity.kt
+++ app/src/main/java/edu/ucne/jorge_moya_ap2_p2/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import dagger.hilt.android.AndroidEntryPoint
+import edu.ucne.jorge_moya_ap2_p2.presentation.navigation.AppNavigationDisplay
 import edu.ucne.jorge_moya_ap2_p2.ui.theme.Jorge_Moya_Ap2_P2Theme
 
 @AndroidEntryPoint
@@ -14,7 +15,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Jorge_Moya_Ap2_P2Theme {
-
+                AppNavigationDisplay()
             }
         }
     }
